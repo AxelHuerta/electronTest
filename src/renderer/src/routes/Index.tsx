faaa11dc57422f06347313e5556d577537caa81e
Axel Huerta <axelhuerta.dev@gmail.com>
2024-02-28 10:56:34 -0600

    add sidebar and tabs

--- src/renderer/src/routes/Index.tsx
+++ src/renderer/src/routes/Index.tsx
@@ -1,33 +1,133 @@
-import Navbar from '@renderer/components/Navbar'
+import { SidebarComponent } from '@renderer/components/Sidebar'
 import { Tabs } from 'keep-react'
 
 function Index() {
   return (
-    <>
-      <Navbar />
-      <div>Home Page</div>
-      <Tabs aria-label="Tabs" style="pills">
-        <Tabs.Item active={true} title="Profile">
-          Profile content
-        </Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Settings">Settings content</Tabs.Item>
-        <Tabs.Item title="Contacts">Contacts content</Tabs.Item>
-        <Tabs.Item disabled={true} title="Disabled">
-          Disabled content
-        </Tabs.Item>
-      </Tabs>
-    </>
+    <div className="bg-red-100">
+      <SidebarComponent />
+      <div className="ml-56">
+        <Tabs aria-label="Tabs" style="pills" className="bg-teal-400 overflow-x-auto">
+          <Tabs.Item active={true} title="Profile" className="bg-purple-100 min-h-screen">
+            <div className="bg-blue-200" style={{ maxWidth: '1000px' }}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+              tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+              eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto amet
+              laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam ea,
+              culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+              ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+              inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+              consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+                tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+                eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto
+                amet laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam
+                ea, culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+                ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+                inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+                consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+                tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+                eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto
+                amet laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam
+                ea, culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+                ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+                inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+                consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+                tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+                eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto
+                amet laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam
+                ea, culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+                ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+                inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+                consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+                tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+                eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto
+                amet laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam
+                ea, culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+                ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+                inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+                consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+                tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+                eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto
+                amet laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam
+                ea, culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+                ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+                inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+                consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, expedita esse
+                tempora fuga soluta accusamus, asperiores voluptas reiciendis ea repudiandae numquam
+                eos. Voluptatibus sapiente, maxime consectetur quae quam alias saepe. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat tempore explicabo architecto
+                amet laboriosam doloremque nam itaque maiores quam molestias ab distinctio quisquam
+                ea, culpa ducimus porro sapiente error dolor. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis facilis, natus distinctio dicta non delectus ad accusantium
+                ipsa totam ipsam, odit, nam fugiat! Esse quisquam cumque molestias ipsam aut fuga!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur suscipit,
+                inventore ut quasi dolorum ab soluta rerum nesciunt necessitatibus. Inventore
+                consectetur veniam commodi modi, a ratione! Est impedit corporis et!
+              </p>
+            </div>
+          </Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
+          <Tabs.Item title="Settings">Settings content</Tabs.Item>
+          <Tabs.Item title="Contacts">Contacts content</Tabs.Item>
+          <Tabs.Item disabled={true} title="Disabled">
+            Disabled content
+          </Tabs.Item>
+        </Tabs>
+      </div>
+    </div>
   )
 }
 
